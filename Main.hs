aac29396128ccdc3dd5a6f31099651e1cf6061b2
josejosepreso <josehihat@gmail.com>
2025-01-31 13:34:03 -0600

    Exclude non plain text files

--- Main.hs
+++ Main.hs
@@ -1,27 +1,32 @@
 import System.Directory
 import System.Environment
-import Control.Exception (catch, SomeException)
+import Control.Exception (try, catch, SomeException)
 import Text.Regex.Posix
-import Data.Either
+import qualified Data.ByteString as B
+import System.Console.ANSI
 
 handler :: SomeException -> IO [a]
 handler e = pure []
 
+lines' path = do
+  content <- B.readFile path
+  if B.isValidUtf8 content then lines <$> readFile path else pure []
+
 grep :: String -> FilePath -> IO [String]
 grep _ [] = pure []
-grep query path = search 1 path <$> (lines <$> readFile path) `catch` handler
+grep query path = search 1 path <$> lines' path `catch` handler
   where search _ _ [] = []
         search i file (x:xs)
-          | x =~ (".*" ++ query ++ ".*") =
-            pure ("FOUND" ++ file ++ ":" ++ show i ++ ": " ++ x)
-            `mappend` search (succ i) file xs
+          | x =~ regex = output `mappend` search (succ i) file xs
           | otherwise = search (succ i) file xs
+            where output = pure $ "FOUND\ESC[91m" ++ file ++ "\ESC[0m:\ESC[32m" ++ show i ++ "\ESC[0m:" ++ x
+                  regex = ".*" ++ query ++ ".*"
 
 find :: [String] -> [FilePath] -> IO [FilePath]
 find [] _ = pure []
 find _ [] = pure []
-find query (x:xs) = (grep (head query) x) `mappend` (find query xs) `mappend` (contents >>= find query) `mappend` contents
-  where contents = fullPath x <$> listDirectory x `catch` handler
+find query (x:xs) = (grep (head query) x) `mappend` (find query xs) `mappend` (dirContents >>= find query) `mappend` dirContents
+  where dirContents = fullPath x <$> listDirectory x `catch` handler
 
 fullPath :: FilePath -> [FilePath] -> [FilePath]
 fullPath root paths = map ((++) (root ++ "/")) paths
